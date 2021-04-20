597d7d1eb666a8b154c8667a2d85655a7ae502c5
ralitsabella <ralicabella@gmail.com>
2021-04-20 13:43:45 +0000

    commit 1

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("home.kpmg.skillstabmaintainor.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
